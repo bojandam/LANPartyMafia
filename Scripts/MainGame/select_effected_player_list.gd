7e9f2560f3f1f5a45e72b18cb292a1c397a3d909
bojandam <bojan.damjanovski1@gmail.com>
2026-03-14 20:57:24 +0100

    Minor setup Names List

--- Scripts/MainGame/select_effected_player_list.gd
+++ Scripts/MainGame/select_effected_player_list.gd
@@ -1,24 +1,20 @@
 extends NameList
 
-@export var player_color:Color =Color(0.173, 0.561, 0.349, 0.471)
-#@export var server_color:Color =Color(0.71, 0.549, 0.141, 0.471)
-
+signal selection_changed(button:BaseButton)
+var selection:String = ""
+var _button_group:ButtonGroup
 
 func _ready() -> void:
 	super._ready()
+	_button_group = _base_children[0].button_group
+	_button_group.pressed.connect(_on_button_pressed)
 	generate(PlayersManager.get_players().values())
-	#name_added.connect(_coloration_check)
-	#
-#
-#func _coloration_check(node:Button):
-	#var set_bg_color = func(color:Color):
-		#var stylebox:StyleBoxFlat = node.get_theme_stylebox("normal").duplicate()
-		#stylebox.bg_color = color
-		#node.add_theme_stylebox_override("normal",stylebox)
-	#if ConnectionManager.player_info["name"] == node.text:
-		#set_bg_color.call(player_color)
-	#
-		
+
+func _on_button_pressed(button:BaseButton):
+	var new_selection: String = button.text
+	if new_selection != selection:
+		selection = new_selection
+		selection_changed.emit(selection)
 
 func generate(names_list:Array[Dictionary]):
 	refresh(names_list)
